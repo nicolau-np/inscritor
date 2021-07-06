839cd26758517362027bb73ab4794048382c6096
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-06 09:05:15 +0100

    criando o metodo para a pagina inicial

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    public function index(){
+        $data = [
+            'title'=>"Inscritor System",
+            'menu'=>"Home",
+            'submenu'=>null,
+            'type'=>"none",
+        ];
+        return view('admin.home', $data);
+    }
 }
