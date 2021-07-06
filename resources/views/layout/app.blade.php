501753406df39b28e32591678721046175ffe288
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-06 09:37:45 +0100

    trabalhando no cabecalho

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -264,7 +264,7 @@
 											<div class="avatar-lg"><img src="{{asset('assets/admin/img/profile.jpg')}}" alt="image profile" class="avatar-img rounded"></div>
 											<div class="u-text">
 												<h4>Hizrian</h4>
-												<p class="text-muted">hello@example.com</p><a href="profile.html" class="btn btn-xs btn-secondary btn-sm">View Profile</a>
+												<p class="text-muted">hello@example.com</p><a href="profile.html" class="btn btn-xs btn-secondary btn-sm">Ver Perfil</a>
 											</div>
 										</div>
 									</li>
