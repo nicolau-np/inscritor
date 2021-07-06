41d2f9864dc38686c8fc55a21f2198693dfb7dd1
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-06 10:03:12 +0100

    removendo o swetalert

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -60,7 +60,7 @@
 										<i class="fa fa-search search-icon"></i>
 									</button>
 								</div>
-								<input type="text" placeholder="Search ..." class="form-control">
+								<input type="text" placeholder="Pesquisar ..." class="form-control">
 							</div>
 						</form>
 					</div>
