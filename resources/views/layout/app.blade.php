0de8da1c9730d00184f35ffb0e561eaeb9a4bd7a
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-06 09:56:09 +0100

    mudando a imagem de perfil

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -254,14 +254,14 @@
 						<li class="nav-item dropdown hidden-caret">
 							<a class="dropdown-toggle profile-pic" data-toggle="dropdown" href="#" aria-expanded="false">
 								<div class="avatar-sm">
-									<img src="{{asset('assets/admin/img/profile.jpg')}}" alt="..." class="avatar-img rounded-circle">
+									<img src="{{asset('assets/neutro/img/no-photo-icon-22.png')}}" alt="..." class="avatar-img rounded-circle">
 								</div>
 							</a>
 							<ul class="dropdown-menu dropdown-user animated fadeIn">
 								<div class="dropdown-user-scroll scrollbar-outer">
 									<li>
 										<div class="user-box">
-											<div class="avatar-lg"><img src="{{asset('assets/admin/img/profile.jpg')}}" alt="image profile" class="avatar-img rounded"></div>
+											<div class="avatar-lg"><img src="{{asset('assets/neutro/img/no-photo-icon-22.png')}}" alt="image profile" class="avatar-img rounded"></div>
 											<div class="u-text">
 												<h4>Hizrian</h4>
 												<p class="text-muted">hello@example.com</p><a href="profile.html" class="btn btn-xs btn-secondary btn-sm">Ver Perfil</a>
@@ -293,7 +293,7 @@
 				<div class="sidebar-content">
 					<div class="user">
 						<div class="avatar-sm float-left mr-2">
-							<img src="{{asset('assets/admin/img/profile.jpg')}}" alt="..." class="avatar-img rounded-circle">
+							<img src="{{asset('assets/neutro/img/no-photo-icon-22.png')}}" alt="..." class="avatar-img rounded-circle">
 						</div>
 						<div class="info">
 							<a data-toggle="collapse" href="#collapseExample" aria-expanded="true">
