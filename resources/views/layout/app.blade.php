8c9ddc92654ca65e50e86a1751ec2984d09c8e68
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-06 09:27:26 +0100

    trabalhando nos menus

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -328,7 +328,7 @@
 					</div>
 					<ul class="nav nav-primary">
 
-                        <li class="nav-item active">
+                        <li class="nav-item @if($type=="home")active @endif">
 							<a href="/">
 								<i class="fas fa-home"></i>
 								<p>Principal</p>
@@ -339,100 +339,39 @@
 							<span class="sidebar-mini-icon">
 								<i class="fa fa-ellipsis-h"></i>
 							</span>
-							<h4 class="text-section">Components</h4>
-						</li>
-						<li class="nav-item">
-							<a data-toggle="collapse" href="#base">
-								<i class="fas fa-layer-group"></i>
-								<p>Base</p>
-								<span class="caret"></span>
+							<h4 class="text-section">Institucional</h4>
+                        </li>
+
+                        <li class="nav-item">
+							<a href="/estudante">
+								<i class="fas fa-user"></i>
+								<p>Estudante</p>
 							</a>
-							<div class="collapse" id="base">
-								<ul class="nav nav-collapse">
-									<li>
-										<a href="components/avatars.html">
-											<span class="sub-item">Avatars</span>
-										</a>
-									</li>
-									<li>
-										<a href="components/buttons.html">
-											<span class="sub-item">Buttons</span>
-										</a>
-									</li>
-									<li>
-										<a href="components/gridsystem.html">
-											<span class="sub-item">Grid System</span>
-										</a>
-									</li>
-									<li>
-										<a href="components/panels.html">
-											<span class="sub-item">Panels</span>
-										</a>
-									</li>
-									<li>
-										<a href="components/notifications.html">
-											<span class="sub-item">Notifications</span>
-										</a>
-									</li>
-									<li>
-										<a href="components/sweetalert.html">
-											<span class="sub-item">Sweet Alert</span>
-										</a>
-									</li>
-									<li>
-										<a href="components/font-awesome-icons.html">
-											<span class="sub-item">Font Awesome Icons</span>
-										</a>
-									</li>
-									<li>
-										<a href="components/simple-line-icons.html">
-											<span class="sub-item">Simple Line Icons</span>
-										</a>
-									</li>
-									<li>
-										<a href="components/flaticons.html">
-											<span class="sub-item">Flaticons</span>
-										</a>
-									</li>
-									<li>
-										<a href="components/typography.html">
-											<span class="sub-item">Typography</span>
-										</a>
-									</li>
-								</ul>
-							</div>
+                        </li>
+
+                        <li class="nav-item">
+							<a href="/estudante">
+								<i class="fas fa-key"></i>
+								<p>Usuários</p>
+							</a>
 						</li>
+
 						<li class="nav-item">
 							<a data-toggle="collapse" href="#sidebarLayouts">
 								<i class="fas fa-th-list"></i>
-								<p>Sidebar Layouts</p>
+								<p>Estatísticas</p>
 								<span class="caret"></span>
 							</a>
 							<div class="collapse" id="sidebarLayouts">
 								<ul class="nav nav-collapse">
 									<li>
 										<a href="sidebar-style-1.html">
-											<span class="sub-item">Sidebar Style 1</span>
+											<span class="sub-item">Inscritos</span>
 										</a>
 									</li>
 									<li>
 										<a href="overlay-sidebar.html">
-											<span class="sub-item">Overlay Sidebar</span>
-										</a>
-									</li>
-									<li>
-										<a href="compact-sidebar.html">
-											<span class="sub-item">Compact Sidebar</span>
-										</a>
-									</li>
-									<li>
-										<a href="static-sidebar.html">
-											<span class="sub-item">Static Sidebar</span>
-										</a>
-									</li>
-									<li>
-										<a href="icon-menu.html">
-											<span class="sub-item">Icon Menu</span>
+											<span class="sub-item">Monetários</span>
 										</a>
 									</li>
 								</ul>
