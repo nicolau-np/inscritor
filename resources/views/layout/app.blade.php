48828f3e7eff3fbe12e49ba3ffae2fc7d4735411
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-06 10:01:02 +0100

    removendo antigo logotipo

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -31,8 +31,8 @@
 			<!-- Logo Header -->
 			<div class="logo-header" data-background-color="blue">
 
-				<a href="index.html" class="logo">
-					<img src="{{asset('assets/admin/img/logo.svg')}}" alt="navbar brand" class="navbar-brand">
+				<a href="/" class="logo">
+					<span style="color:#fff; font-weight:bold;">Inscritor</span>
 				</a>
 				<button class="navbar-toggler sidenav-toggler ml-auto" type="button" data-toggle="collapse" data-target="collapse" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon">
@@ -989,7 +989,7 @@
 		</div>
 
 		<!-- Custom template | don't include it in your project! -->
-		
+
 		<!-- End Custom template -->
 	</div>
 	<!--   Core JS Files   -->
