a06d9bfc5d71a786991bbaad9811332cb96a87fd
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-06 09:17:36 +0100

    chamando todas as dependencias dos js e css para o nosso template

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -4,14 +4,14 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <title>{{$title}}</title>
 	<meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
-	<link rel="icon" href="../assets/img/icon.ico" type="image/x-icon"/>
+	<link rel="icon" href="{{asset('assets/admin/img/icon.ico')}}" type="image/x-icon"/>
 
 	<!-- Fonts and icons -->
-	<script src="../assets/js/plugin/webfont/webfont.min.js"></script>
+	<script src="{{asset('assets/admin/js/plugin/webfont/webfont.min.js')}}"></script>
 	<script>
 		WebFont.load({
 			google: {"families":["Lato:300,400,700,900"]},
-			custom: {"families":["Flaticon", "Font Awesome 5 Solid", "Font Awesome 5 Regular", "Font Awesome 5 Brands", "simple-line-icons"], urls: ['../assets/css/fonts.min.css']},
+			custom: {"families":["Flaticon", "Font Awesome 5 Solid", "Font Awesome 5 Regular", "Font Awesome 5 Brands", "simple-line-icons"], urls: ['{{asset('assets/admin/css/fonts.min.css')}}']},
 			active: function() {
 				sessionStorage.fonts = true;
 			}
@@ -19,11 +19,11 @@
 	</script>
 
 	<!-- CSS Files -->
-	<link rel="stylesheet" href="../assets/css/bootstrap.min.css">
-	<link rel="stylesheet" href="../assets/css/atlantis.min.css">
+	<link rel="stylesheet" href="{{asset('assets/admin/css/bootstrap.min.css')}}">
+	<link rel="stylesheet" href="{{asset('assets/admin/css/atlantis.min.css')}}">
 
 	<!-- CSS Just for demo purpose, don't include it in your project -->
-	<link rel="stylesheet" href="../assets/css/demo.css">
+	<link rel="stylesheet" href="{{asset('assets/admin/css/demo.css')}}">
 </head>
 <body>
 	<div class="wrapper">
@@ -32,7 +32,7 @@
 			<div class="logo-header" data-background-color="blue">
 
 				<a href="index.html" class="logo">
-					<img src="../assets/img/logo.svg" alt="navbar brand" class="navbar-brand">
+					<img src="{{asset('assets/admin/img/logo.svg')}}" alt="navbar brand" class="navbar-brand">
 				</a>
 				<button class="navbar-toggler sidenav-toggler ml-auto" type="button" data-toggle="collapse" data-target="collapse" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon">
@@ -86,7 +86,7 @@
 										<div class="notif-center">
 											<a href="#">
 												<div class="notif-img">
-													<img src="../assets/img/jm_denis.jpg" alt="Img Profile">
+													<img src="{{asset('assets/admin/img/jm_denis.jpg')}}" alt="Img Profile">
 												</div>
 												<div class="notif-content">
 													<span class="subject">Jimmy Denis</span>
@@ -98,7 +98,7 @@
 											</a>
 											<a href="#">
 												<div class="notif-img">
-													<img src="../assets/img/chadengle.jpg" alt="Img Profile">
+													<img src="{{asset('assets/admin/img/chadengle.jpg')}}" alt="Img Profile">
 												</div>
 												<div class="notif-content">
 													<span class="subject">Chad</span>
@@ -110,7 +110,7 @@
 											</a>
 											<a href="#">
 												<div class="notif-img">
-													<img src="../assets/img/mlane.jpg" alt="Img Profile">
+													<img src="{{asset('assets/admin/img/mlane.jpg')}}" alt="Img Profile">
 												</div>
 												<div class="notif-content">
 													<span class="subject">Jhon Doe</span>
@@ -122,7 +122,7 @@
 											</a>
 											<a href="#">
 												<div class="notif-img">
-													<img src="../assets/img/talha.jpg" alt="Img Profile">
+													<img src="{{asset('assets/admin/img/talha.jpg')}}" alt="Img Profile">
 												</div>
 												<div class="notif-content">
 													<span class="subject">Talha</span>
@@ -172,7 +172,7 @@
 											</a>
 											<a href="#">
 												<div class="notif-img">
-													<img src="../assets/img/profile2.jpg" alt="Img Profile">
+													<img src="{{asset('assets/admin/img/profile2.jpg')}}" alt="Img Profile">
 												</div>
 												<div class="notif-content">
 													<span class="block">
@@ -254,14 +254,14 @@
 						<li class="nav-item dropdown hidden-caret">
 							<a class="dropdown-toggle profile-pic" data-toggle="dropdown" href="#" aria-expanded="false">
 								<div class="avatar-sm">
-									<img src="../assets/img/profile.jpg" alt="..." class="avatar-img rounded-circle">
+									<img src="{{asset('assets/admin/img/profile.jpg')}}" alt="..." class="avatar-img rounded-circle">
 								</div>
 							</a>
 							<ul class="dropdown-menu dropdown-user animated fadeIn">
 								<div class="dropdown-user-scroll scrollbar-outer">
 									<li>
 										<div class="user-box">
-											<div class="avatar-lg"><img src="../assets/img/profile.jpg" alt="image profile" class="avatar-img rounded"></div>
+											<div class="avatar-lg"><img src="{{asset('assets/admin/img/profile.jpg')}}" alt="image profile" class="avatar-img rounded"></div>
 											<div class="u-text">
 												<h4>Hizrian</h4>
 												<p class="text-muted">hello@example.com</p><a href="profile.html" class="btn btn-xs btn-secondary btn-sm">View Profile</a>
@@ -293,7 +293,7 @@
 				<div class="sidebar-content">
 					<div class="user">
 						<div class="avatar-sm float-left mr-2">
-							<img src="../assets/img/profile.jpg" alt="..." class="avatar-img rounded-circle">
+							<img src="{{asset('assets/admin/img/profile.jpg')}}" alt="..." class="avatar-img rounded-circle">
 						</div>
 						<div class="info">
 							<a data-toggle="collapse" href="#collapseExample" aria-expanded="true">
@@ -738,7 +738,7 @@
 														<tr>
 															<td>
 																<div class="flag">
-																	<img src="../assets/img/flags/id.png" alt="indonesia">
+																	<img src="{{asset('assets/admin/img/flags/id.png')}}" alt="indonesia">
 																</div>
 															</td>
 															<td>Indonesia</td>
@@ -752,7 +752,7 @@
 														<tr>
 															<td>
 																<div class="flag">
-																	<img src="../assets/img/flags/us.png" alt="united states">
+																	<img src="{{asset('assets/admin/img/flags/us.png')}}" alt="united states">
 																</div>
 															</td>
 															<td>USA</td>
@@ -766,7 +766,7 @@
 														<tr>
 															<td>
 																<div class="flag">
-																	<img src="../assets/img/flags/au.png" alt="australia">
+																	<img src="{{asset('assets/admin/img/flags/au.png')}}" alt="australia">
 																</div>
 															</td>
 															<td>Australia</td>
@@ -780,7 +780,7 @@
 														<tr>
 															<td>
 																<div class="flag">
-																	<img src="../assets/img/flags/ru.png" alt="russia">
+																	<img src="{{asset('assets/admin/img/flags/ru.png')}}" alt="russia">
 																</div>
 															</td>
 															<td>Russia</td>
@@ -794,7 +794,7 @@
 														<tr>
 															<td>
 																<div class="flag">
-																	<img src="../assets/img/flags/cn.png" alt="china">
+																	<img src="{{asset('assets/admin/img/flags/cn.png')}}" alt="china">
 																</div>
 															</td>
 															<td>China</td>
@@ -808,7 +808,7 @@
 														<tr>
 															<td>
 																<div class="flag">
-																	<img src="../assets/img/flags/br.png" alt="brazil">
+																	<img src="{{asset('assets/admin/img/flags/br.png')}}" alt="brazil">
 																</div>
 															</td>
 															<td>Brasil</td>
@@ -842,7 +842,7 @@
 								<div class="card-body pb-0">
 									<div class="d-flex">
 										<div class="avatar">
-											<img src="../assets/img/logoproduct.svg" alt="..." class="avatar-img rounded-circle">
+											<img src="{{asset('assets/admin/img/logoproduct.svg')}}" alt="..." class="avatar-img rounded-circle">
 										</div>
 										<div class="flex-1 pt-1 ml-2">
 											<h6 class="fw-bold mb-1">CSS</h6>
@@ -855,7 +855,7 @@
 									<div class="separator-dashed"></div>
 									<div class="d-flex">
 										<div class="avatar">
-											<img src="../assets/img/logoproduct.svg" alt="..." class="avatar-img rounded-circle">
+											<img src="{{asset('assets/admin/img/logoproduct.svg')}}" alt="..." class="avatar-img rounded-circle">
 										</div>
 										<div class="flex-1 pt-1 ml-2">
 											<h6 class="fw-bold mb-1">J.CO Donuts</h6>
@@ -868,7 +868,7 @@
 									<div class="separator-dashed"></div>
 									<div class="d-flex">
 										<div class="avatar">
-											<img src="../assets/img/logoproduct3.svg" alt="..." class="avatar-img rounded-circle">
+											<img src="{{asset('assets/admin/img/logoproduct3.svg')}}" alt="..." class="avatar-img rounded-circle">
 										</div>
 										<div class="flex-1 pt-1 ml-2">
 											<h6 class="fw-bold mb-1">Ready Pro</h6>
@@ -892,7 +892,7 @@
 									<div class="card-list">
 										<div class="item-list">
 											<div class="avatar">
-												<img src="../assets/img/jm_denis.jpg" alt="..." class="avatar-img rounded-circle">
+												<img src="{{asset('assets/admin/img/jm_denis.jpg')}}" alt="..." class="avatar-img rounded-circle">
 											</div>
 											<div class="info-user ml-3">
 												<div class="username">Jimmy Denis</div>
@@ -904,7 +904,7 @@
 										</div>
 										<div class="item-list">
 											<div class="avatar">
-												<img src="../assets/img/chadengle.jpg" alt="..." class="avatar-img rounded-circle">
+												<img src="{{asset('assets/admin/img/chadengle.jpg')}}" alt="..." class="avatar-img rounded-circle">
 											</div>
 											<div class="info-user ml-3">
 												<div class="username">Chad</div>
@@ -916,7 +916,7 @@
 										</div>
 										<div class="item-list">
 											<div class="avatar">
-												<img src="../assets/img/talha.jpg" alt="..." class="avatar-img rounded-circle">
+												<img src="{{asset('assets/admin/img/talha.jpg')}}" alt="..." class="avatar-img rounded-circle">
 											</div>
 											<div class="info-user ml-3">
 												<div class="username">Talha</div>
@@ -928,7 +928,7 @@
 										</div>
 										<div class="item-list">
 											<div class="avatar">
-												<img src="../assets/img/mlane.jpg" alt="..." class="avatar-img rounded-circle">
+												<img src="{{asset('assets/admin/img/mlane.jpg')}}" alt="..." class="avatar-img rounded-circle">
 											</div>
 											<div class="info-user ml-3">
 												<div class="username">John Doe</div>
@@ -940,7 +940,7 @@
 										</div>
 										<div class="item-list">
 											<div class="avatar">
-												<img src="../assets/img/talha.jpg" alt="..." class="avatar-img rounded-circle">
+												<img src="{{asset('assets/admin/img/talha.jpg')}}" alt="..." class="avatar-img rounded-circle">
 											</div>
 											<div class="info-user ml-3">
 												<div class="username">Talha</div>
@@ -952,7 +952,7 @@
 										</div>
 										<div class="item-list">
 											<div class="avatar">
-												<img src="../assets/img/jm_denis.jpg" alt="..." class="avatar-img rounded-circle">
+												<img src="{{asset('assets/admin/img/jm_denis.jpg')}}" alt="..." class="avatar-img rounded-circle">
 											</div>
 											<div class="info-user ml-3">
 												<div class="username">Jimmy Denis</div>
@@ -1210,46 +1210,46 @@
 		<!-- End Custom template -->
 	</div>
 	<!--   Core JS Files   -->
-	<script src="../assets/js/core/jquery.3.2.1.min.js"></script>
-	<script src="../assets/js/core/popper.min.js"></script>
-	<script src="../assets/js/core/bootstrap.min.js"></script>
+	<script src="{{asset('assets/admin/js/core/jquery.3.2.1.min.js')}}"></script>
+	<script src="{{asset('assets/admin/js/core/popper.min.js')}}"></script>
+	<script src="{{asset('assets/admin/js/core/bootstrap.min.js')}}"></script>
 
 	<!-- jQuery UI -->
-	<script src="../assets/js/plugin/jquery-ui-1.12.1.custom/jquery-ui.min.js"></script>
-	<script src="../assets/js/plugin/jquery-ui-touch-punch/jquery.ui.touch-punch.min.js"></script>
+	<script src="{{asset('assets/admin/js/plugin/jquery-ui-1.12.1.custom/jquery-ui.min.js')}}"></script>
+	<script src="{{asset('assets/admin/js/plugin/jquery-ui-touch-punch/jquery.ui.touch-punch.min.js')}}"></script>
 
 	<!-- jQuery Scrollbar -->
-	<script src="../assets/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js"></script>
+	<script src="{{asset('assets/admin/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js')}}"></script>
 
 
 	<!-- Chart JS -->
-	<script src="../assets/js/plugin/chart.js/chart.min.js"></script>
+	<script src="{{asset('assets/admin/js/plugin/chart.js')}}/chart.min.js')}}"></script>
 
 	<!-- jQuery Sparkline -->
-	<script src="../assets/js/plugin/jquery.sparkline/jquery.sparkline.min.js"></script>
+	<script src="{{asset('assets/admin/js/plugin/jquery.sparkline/jquery.sparkline.min.js')}}"></script>
 
 	<!-- Chart Circle -->
-	<script src="../assets/js/plugin/chart-circle/circles.min.js"></script>
+	<script src="{{asset('assets/admin/js/plugin/chart-circle/circles.min.js')}}"></script>
 
 	<!-- Datatables -->
-	<script src="../assets/js/plugin/datatables/datatables.min.js"></script>
+	<script src="{{asset('assets/admin/js/plugin/datatables/datatables.min.js')}}"></script>
 
 	<!-- Bootstrap Notify -->
-	<script src="../assets/js/plugin/bootstrap-notify/bootstrap-notify.min.js"></script>
+	<script src="{{asset('assets/admin/js/plugin/bootstrap-notify/bootstrap-notify.min.js')}}"></script>
 
 	<!-- jQuery Vector Maps -->
-	<script src="../assets/js/plugin/jqvmap/jquery.vmap.min.js"></script>
-	<script src="../assets/js/plugin/jqvmap/maps/jquery.vmap.world.js"></script>
+	<script src="{{asset('assets/admin/js/plugin/jqvmap/jquery.vmap.min.js')}}"></script>
+	<script src="{{asset('assets/admin/js/plugin/jqvmap/maps/jquery.vmap.world.js')}}"></script>
 
 	<!-- Sweet Alert -->
-	<script src="../assets/js/plugin/sweetalert/sweetalert.min.js"></script>
+	<script src="{{asset('assets/admin/js/plugin/sweetalert/sweetalert.min.js')}}"></script>
 
 	<!-- Atlantis JS -->
-	<script src="../assets/js/atlantis.min.js"></script>
+	<script src="{{asset('assets/admin/js/atlantis.min.js')}}"></script>
 
 	<!-- Atlantis DEMO methods, don't include it in your project! -->
-	<script src="../assets/js/setting-demo.js"></script>
-	<script src="../assets/js/demo.js"></script>
+	<script src="{{asset('assets/admin/js/setting-demo.js')}}"></script>
+	<script src="{{asset('assets/admin/js/demo.js')}}"></script>
 	<script>
 		Circles.create({
 			id:'circles-1',
