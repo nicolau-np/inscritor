0935824ffcf972384dfa377fac2f09b30b73253d
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-09 09:48:29 +0100

    eliminando todos os modelos

--- resources/views/admin/user/login.blade.php
+++ resources/views/admin/user/login.blade.php
@@ -1,10 +1,24 @@
 @extends('layout.app')
 @section('content')
 <div class="container">
-    
+
     <div class="form">
         {{Form::open(['method'=>"post", 'url'=>""])}}
+        <div class="row">
+            <div class="col-md-12">
+                <div class="form-group">
+                    {{Form::email('email', null, ['placeholder'=>"Email", 'class'=>"form-controll"])}}
 
+                </div>
+            </div>
+            <div class="col-md-12">
+                <div class="form-group">
+                    {{Form::label('password')}}
+                    {{Form::password('password', null, ['placeholder'=>"Palavra-Passe", 'class'=>"form-controll"])}}
+
+                </div>
+            </div>
+        </div>
         {{Form::close()}}
     </div>
 </div>
