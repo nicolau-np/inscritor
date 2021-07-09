8d9bd4760927c76bae65e5815de2de1ae4c54844
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-09 22:25:09 +0100

    trabalahndo modelo Instituicao

--- app/Instituicao.php
+++ app/Instituicao.php
@@ -6,5 +6,31 @@ use Illuminate\Database\Eloquent\Model;
 
 class Instituicao extends Model
 {
-    //
+    protected $table = "instituicaos";
+
+    protected $fillable = [
+        'nome',
+        'bairro',
+        'estado',
+    ];
+
+    public function usuario(){
+        return $this->hasMany(User::class, 'id_instituicao', 'id');
+    }
+
+    public function estudante(){
+        return $this->hasMany(Estudante::class, 'id_instituicao', 'id');
+    }
+
+    public function condicao(){
+        return $this->hasMany(Condicao::class, 'id_instituicao', 'id');
+    }
+
+    public function nivel_instituicao(){
+        return $this->belongsTo(NivelInstituicao::class, 'id_nivel_instituicao', 'id');
+    }
+
+    public function tipo_instituicao(){
+        return $this->belongsTo(TipoInstituicao::class, 'id_tipo_instituicao', 'id');
+    }
 }
