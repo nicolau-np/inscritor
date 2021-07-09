8a56b831682ca0bc0932acdccbb5c5b4b48e5143
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-09 09:40:49 +0100

    terminado migration estudantes

--- database/migrations/2021_07_09_090244_create_estudantes_table.php
+++ database/migrations/2021_07_09_090244_create_estudantes_table.php
@@ -14,8 +14,23 @@ class CreateEstudantesTable extends Migration
     public function up()
     {
         Schema::create('estudantes', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            $table->bigInteger('id_pessoa')->unsigned()->index();
+            $table->bigInteger('id_instituicao')->unsigned()->index();
+            $table->bigInteger('id_curso')->unsigned()->index();
+            $table->bigInteger('id_classe')->unsigned()->index();
+            $table->bigInteger('id_turno')->unsigned()->index();
+            $table->bigInteger('id_ano_lectivo')->unsigned()->index();
             $table->timestamps();
+        });
+
+        Schema::table('estudantes', function (Blueprint $table) {
+            $table->foreign('id_pessoa')->references('id')->on('pessoas')->onUpdate('cascade');
+            $table->foreign('id_instituicao')->references('id')->on('instituicaos')->onUpdate('cascade');
+            $table->foreign('id_curso')->references('id')->on('cursos')->onUpdate('cascade');
+            $table->foreign('id_classe')->references('id')->on('classes')->onUpdate('cascade');
+            $table->foreign('id_turno')->references('id')->on('turnos')->onUpdate('cascade');
+            $table->foreign('id_ano_lectivo')->references('id')->on('ano_lectivos')->onUpdate('cascade');
         });
     }
 
