df3cda9c8bf1093042a80df4cdcea79aaefab9fc
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-09 22:51:19 +0100

    trabalahndo modelo TipoDocs

--- app/TipoDoc.php
+++ app/TipoDoc.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class TipoDoc extends Model
 {
-    //
+    protected $table = "tipo_docs";
+
+    protected $fillable = [
+        'tipo',
+        'estado',
+    ];
+
+    public function documento(){
+        return $this->hasMany(Doc::class, 'id_tipo_doc', 'id');
+    }
 }
