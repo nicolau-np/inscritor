ed39dc3edb3edd29e00ab74ea62474d5607e5d5a
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-09 22:17:27 +0100

    trabalahndo modelo TipoInstituicao

--- app/TipoInstituicao.php
+++ app/TipoInstituicao.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class TipoInstituicao extends Model
 {
-    //
+    protected $table = "tipo_instituicaos";
+
+    protected $fillable = [
+        'tipo',
+        'estado',
+    ];
+
+    public function instituicao(){
+        return $this->hasMany(Instituicao::class, 'id_tipo_instituicao', 'id');
+    }
 }
