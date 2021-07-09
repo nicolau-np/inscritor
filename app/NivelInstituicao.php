819fe2c5aa2a79ce10c72978514fd7a99a274c62
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-09 22:19:41 +0100

    trabalahndo modelo NivelInstituicao

--- app/NivelInstituicao.php
+++ app/NivelInstituicao.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class NivelInstituicao extends Model
 {
-    //
+    protected $table = "nivel_instituicaos";
+
+    protected $fillable = [
+        'nivel',
+        'estado',
+    ];
+
+    public function instituicao(){
+        return $this->hasMany(Instituicao::class, 'id_nivel_instituicao', 'id');
+    }
 }
