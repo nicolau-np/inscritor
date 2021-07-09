8b572c02696c173a514fc811014ce4eba955259e
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-09 22:52:48 +0100

    trabalahndo modelo Docs

--- app/Doc.php
+++ app/Doc.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Doc extends Model
 {
-    //
+    protected $table = "docs";
+
+    protected $fillable = [
+        'id_tipo_doc',
+        'id_estudante',
+        'estado',
+    ];
+
+    public function tipo_doc(){
+        return $this->belongsTo(TipoDoc::class, 'id_tipo_doc', 'id');
+    }
 }
