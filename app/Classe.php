1fc779f434958f9299b3b26034be3df9d85875f3
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-09 22:35:10 +0100

    trabalahndo modelo Classe

--- app/Classe.php
+++ app/Classe.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Classe extends Model
 {
-    //
+    protected $table = "classes";
+
+    protected $fillable = [
+        'classe',
+        'estado',
+    ];
+
+    public function estudante(){
+        return $this->hasMany(Estudante::class, 'id_classe', 'id');
+    }
 }
