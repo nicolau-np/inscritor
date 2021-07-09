a75422356c30c704ec0902c7d57c35043b9778c1
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-09 22:55:44 +0100

    trabalahndo modelo Condicao

--- app/Condicao.php
+++ app/Condicao.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Condicao extends Model
 {
-    //
+    protected $table = "condicaos";
+
+    protected $fillable = [
+       'id_instituicao',
+       'id_ano_lectivo',
+       'ano_inicio',
+       'ano_fim',
+        'estado',
+    ];
+
+    public function instituicao(){
+        return $this->belongsTo(Instituicao::class, 'id_instituicao', 'id');
+    }
+
+    public function ano_lectivo(){
+        return $this->belongsTo(AnoLectivo::class, 'id_ano_lectivo', 'id');
+    }
 }
