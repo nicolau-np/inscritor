b53ce525d63ce8f914d3f9837543d2bd50f99f41
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-09 09:26:00 +0100

    terminado migration usuario

--- database/migrations/2021_07_09_090153_create_usuarios_table.php
+++ database/migrations/2021_07_09_090153_create_usuarios_table.php
@@ -14,8 +14,19 @@ class CreateUsuariosTable extends Migration
     public function up()
     {
         Schema::create('usuarios', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            $table->bigInteger('id_pessoa')->unsigned()->index();
+            $table->bigInteger('id_instituicao')->unsigned()->index();
+            $table->string('email')->unique();
+            $table->text('palavra_passe');
+            $table->string('nivel_acesso');
+            $table->string('estado');
             $table->timestamps();
+        });
+
+        Schema::table('usuarios', function (Blueprint $table) {
+            $table->foreign('id_pessoa')->references('id')->on('pessoas')->onUpdate('cascade');
+            $table->foreign('id_instituicao')->references('id')->on('instituicaos')->onUpdate('cascade');
         });
     }
 
