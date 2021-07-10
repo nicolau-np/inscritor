99033eb1da4cb16ba5d76c99d4d86710c9be83a7
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-10 12:16:04 +0100

    terminado seeder anoLectivo

--- database/seeds/AnoLectivoSeeder.php
+++ database/seeds/AnoLectivoSeeder.php
@@ -1,16 +1,23 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class AnoLectivoSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
+    static $ano_lectivos = [
+        [
+            'ano_lectivo'=>"2021 - 2022",
+            'estado'=>"on",
+        ]
+    ];
+
     public function run()
     {
-        //
+        foreach(Self::$ano_lectivos as $ano_lectivo){
+            DB::table('ano_lectivos')->insert(
+                $ano_lectivo
+            );
+        }
     }
 }
