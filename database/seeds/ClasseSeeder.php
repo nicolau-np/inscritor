4b01f3744fbb447358ac31dd0488a1e8fd4b439f
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-10 12:11:36 +0100

    terminado seeder classe

--- database/seeds/ClasseSeeder.php
+++ database/seeds/ClasseSeeder.php
@@ -1,16 +1,32 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ClasseSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
+    static $classes = [
+        [
+            'classe'=>"10ª",
+            'estado'=>"on",
+        ],[
+            'classe'=>"11ª",
+            'estado'=>"on",
+        ],[
+            'classe'=>"12ª",
+            'estado'=>"on",
+        ],[
+            'classe'=>"13ª",
+            'estado'=>"on",
+        ]
+    ];
+
     public function run()
     {
-        //
+        foreach(Self::$classes as $classe){
+            DB::table('classes')->insert(
+                $classe
+            );
+        }
     }
 }
