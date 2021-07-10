56d5af6446c6fba8b1f97b3401cb6141b75fdf4a
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-10 12:42:28 +0100

    update instituicao migrate

--- database/migrations/2021_07_09_090130_create_instituicaos_table.php
+++ database/migrations/2021_07_09_090130_create_instituicaos_table.php
@@ -15,10 +15,17 @@ class CreateInstituicaosTable extends Migration
     {
         Schema::create('instituicaos', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->bigInteger('id_tipo_instituicao')->unsigned()->index();
+            $table->bigInteger('id_nivel_instituicao')->unsigned()->index();
             $table->string('nome');
             $table->string('bairro');
             $table->string('estado');
             $table->timestamps();
+        });
+
+        Schema::table('instituicaos', function (Blueprint $table) {
+            $table->foreign('id_tipo_instituicao')->references('id')->on('tipo_instituicaos')->onUpdate('cascade');
+            $table->foreign('id_nivel_instituicao')->references('id')->on('nivel_instituicaos')->onUpdate('cascade');
         });
     }
 
