90d432683bf30ba10c8e9724e4545b24f676d791
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-10 12:07:00 +0100

    terminado seeder usuario

--- database/seeds/UsuarioSeeder.php
+++ database/seeds/UsuarioSeeder.php
@@ -1,21 +1,36 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UsuarioSeeder extends Seeder
 {
-    static $usuarios = [
-        [
-            'id_pessoa'=>,
-            'id_instituicao',
-            'email',
-            'palavra_passe',
-            'nivel_acesso',
-            'estado',
-        ]
-    ];
+
     public function run()
     {
-        //
+        DB::table('usuarios')->insert(
+            array(
+
+                array(
+                    'id_pessoa' => 1,
+                    'id_instituicao' => 1,
+                    'email' => "nic340@gmail.com",
+                    'palavra_passe' => Hash::make("olamundo2015"),
+                    'nivel_acesso' => "master",
+                    'estado' => "on",
+                ),
+                array(
+                    'id_pessoa' => 2,
+                    'id_instituicao' => 2,
+                    'email' => "armindadoresd@gmail.com",
+                    'palavra_passe' => Hash::make("babaca"),
+                    'nivel_acesso' => "admin",
+                    'estado' => "on",
+                ),
+
+            )
+
+        );
     }
 }
