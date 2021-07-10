3242c36c7f2fe470b40652007d4b3ef6195a8b3c
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-10 12:00:44 +0100

    terminado seeder pessoa

--- database/seeds/PessoaSeeder.php
+++ database/seeds/PessoaSeeder.php
@@ -1,16 +1,26 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class PessoaSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
+    static $pessoas = [
+        [
+            'nome' => "Nicolau Ngala Pungue",
+            'bi' => "005581463NE046",
+            'telefone' => 946216795,
+            'genero' => "M",
+            'data_nascimento' => "1996-08-29",
+        ],
+    ];
+
     public function run()
     {
-        //
+        foreach (Self::$pessoas as $pessoa) {
+            DB::table('pessoas')->insert(
+                $pessoa
+            );
+        }
     }
 }
