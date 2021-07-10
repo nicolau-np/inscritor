ae7295b65d05bcda6367601a29c162029839ad09
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-10 11:55:43 +0100

    terminado seeder instituicao

--- database/seeds/InstituicaoSeeder.php
+++ database/seeds/InstituicaoSeeder.php
@@ -4,9 +4,27 @@ use Illuminate\Database\Seeder;
 
 class InstituicaoSeeder extends Seeder
 {
-    
+    static $instituicaos = [
+        [
+
+            'id_tipo_instituicao' =>1,
+            'id_nivel_instituicao' =>1,
+            'nome'=>"Master",
+            'bairro'=>"master",
+            'estado' =>"on",
+        ],
+        [
+
+            'id_tipo_instituicao' =>2,
+            'id_nivel_instituicao' =>2,
+            'nome'=>"Liceu Welwitchia Mirabilis",
+            'bairro'=>"Eucalíptos",
+            'estado' =>"on",
+        ]
+    ];
+
     public function run()
     {
-        //
+
     }
 }
