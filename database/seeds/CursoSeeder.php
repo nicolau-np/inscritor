4626f561748d8c4c2556f307b21c12e394f35b52
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-10 12:09:19 +0100

    terminado seeder curso

--- database/seeds/CursoSeeder.php
+++ database/seeds/CursoSeeder.php
@@ -1,16 +1,27 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class CursoSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
+    static $cursos = [
+        [
+            'curso' => "Ciências Físicas e Biologica",
+            'estado' => "on",
+        ],
+        [
+            'curso' => "Educação Plástica",
+            'estado' => "on",
+        ],
+    ];
+
     public function run()
     {
-        //
+        foreach (Self::$cursos as $curso) {
+            DB::table('cursos')->insert(
+                $curso
+            );
+        }
     }
 }
