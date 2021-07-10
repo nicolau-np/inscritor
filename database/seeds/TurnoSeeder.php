58dcedb2954633f84bed822edda6bf38b5d7caa1
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-10 12:13:46 +0100

    terminado seeder turno

--- database/seeds/TurnoSeeder.php
+++ database/seeds/TurnoSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class TurnoSeeder extends Seeder
 {
@@ -8,16 +9,20 @@ class TurnoSeeder extends Seeder
         [
             'turno' => "Manha",
             'estado' => "on",
-        ],[
+        ], [
             'turno' => "Tarde",
             'estado' => "on",
-        ],[
+        ], [
             'turno' => "Noite",
             'estado' => "on",
         ],
     ];
     public function run()
     {
-        //
+        foreach (Self::$turnos as $turno) {
+            DB::table('turnos')->insert(
+                $turno
+            );
+        }
     }
 }
