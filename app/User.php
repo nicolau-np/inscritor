305847dad1554d55e6c63ad11ac7ae51e90e7f6f
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-11 09:57:33 +0100

    mudando o campo de palavra passe no modelo

--- app/User.php
+++ app/User.php
@@ -21,28 +21,11 @@ class User extends Authenticatable
         'id_pessoa',
         'id_instituicao',
         'email',
-        'palavra_passe',
+        'password',
         'nivel_acesso',
         'estado',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
 
     public function instituicao(){
         return $this->belongsTo(Instituicao::class, 'id_instituicao', 'id');
