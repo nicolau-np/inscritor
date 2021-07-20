db3759333d99aa8bcf2f72bf5d3043dd47246f12
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-20 08:10:58 +0100

    update

--- resources/views/admin/estudante/create.blade.php
+++ resources/views/admin/estudante/create.blade.php
@@ -31,43 +31,6 @@
                    </div>
                </div>
 
-
-
-
-<label class="mt-3 mb-3"><b>Form Floating Label</b></label>
-<div class="form-group form-floating-label">
-	<input id="inputFloatingLabel" type="text" class="form-control input-border-bottom" required="">
-	<label for="inputFloatingLabel" class="placeholder">Input</label>
-</div>
-
-<div class="form-group form-floating-label">
-	<select class="form-control input-border-bottom" id="selectFloatingLabel" required="">
-		<option></option>
-		<option>1</option>
-		<option>2</option>
-		<option>3</option>
-		<option>4</option>
-		<option>5</option>
-	</select>
-	<label for="selectFloatingLabel" class="placeholder">Select</label>
-</div>
-
-<div class="form-group form-floating-label">
-	<input id="inputFloatingLabel2" type="text" class="form-control input-solid" required="">
-	<label for="inputFloatingLabel2" class="placeholder">Input</label>
-</div>
-
-<div class="form-group form-floating-label">
-	<select class="form-control input-solid" id="selectFloatingLabel2" required="">
-		<option></option>
-		<option>1</option>
-		<option>2</option>
-		<option>3</option>
-		<option>4</option>
-		<option>5</option>
-	</select>
-	<label for="selectFloatingLabel2" class="placeholder">Select</label>
-</div>
             </div>
 
         </div>
