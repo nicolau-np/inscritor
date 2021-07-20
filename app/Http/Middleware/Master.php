b91cfc0253b9f78658b98a07f78814b28ff7f19a
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-20 08:51:20 +0100

    trabalhar na middleware do admin

--- app/Http/Middleware/Master.php
+++ app/Http/Middleware/Master.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Auth;
 
 class Master
 {
@@ -15,6 +16,9 @@ class Master
      */
     public function handle($request, Closure $next)
     {
-        return $next($request);
+        if(Auth::check() && Auth::user()->acesso == "master"){
+            return $next($request);
+        }
+        return redirect('/admin/');
     }
 }
