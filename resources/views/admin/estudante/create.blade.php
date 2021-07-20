7add69c032ce9403e3c4ff3229c11e22dc02da30
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-20 07:58:45 +0100

    criando estrutura da pagina de criar estudante

---
+++ resources/views/admin/estudante/create.blade.php
@@ -0,0 +1,16 @@
+@extends('layout.app')
+
+@section('content')
+<div class="col-md-12">
+    <div class="card">
+        <div class="carde-header">
+            <div class="card-title">
+                title
+            </div>
+        </div>
+        <div class="card-body">
+            body
+        </div>
+    </div>
+</div>
+@endsection
