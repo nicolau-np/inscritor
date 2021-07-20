be6706acf1c81413faad95d065d4e06bcc5a1e60
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-20 08:54:06 +0100

    Adicionando a middleware admin no kernel e dando um nome a ela

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -54,6 +54,7 @@ class Kernel extends HttpKernel
      */
     protected $routeMiddleware = [
         'master' => \App\Http\Middleware\Master::class,
+        'admin' => \App\Http\Middleware\Admin::class,
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
