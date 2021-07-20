87ae71637980213739ae4834ff3bc8218eade617
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-20 14:48:01 +0100

    criando form de curso

---
+++ resources/views/admin/curso/create.blade.php
@@ -0,0 +1,61 @@
+@extends('layout.app')
+@section('content')
+<div class="col-md-12">
+    <div class="card">
+        <div class="card-header">
+        <div class="card-title">{{$submenu}}</div>
+            <div class="card-category">
+            <a href="/admin/cursos/" class="btn btn-success">Listar</a>
+            </div>
+        </div>
+        <div class="card-body">
+
+            <div class="form">
+
+                @if (session('error'))
+                    <div class="alert bg-danger" role="alert"><em class="fa fa-lg fa-warning">&nbsp;</em> {{__(session('error'))}} <a href="#" class="pull-right"><em class="fa fa-lg fa-close"></em></a></div>
+                @endif
+
+                @if (session('success'))
+                    <div class="alert bg-success" role="alert"><em class="fa fa-lg fa-check">&nbsp;</em> {{session('success')}} <a href="#" class="pull-right"><em class="fa fa-lg fa-close"></em></a></div>
+                @endif
+
+                {{Form::open(['method'=>"post", 'url' =>"/admin/cursos"])}}
+               <div class="row">
+                   <div class="col-md-4">
+                       <div class="form-group form-group-default">
+                       <label>Curso<span class="text-danger">*</span></label>
+                       {{Form::number('curso', null, ['class'=>"form-control", 'placeholder'=>"Curso"])}}
+                       @if($errors->has('curso'))
+                        <span class="text-danger">{{$errors->first('curso')}}</span>
+                        @endif
+                       </div>
+                   </div>
+
+                   <div class="col-md-4">
+                    <div class="form-group form-group-default">
+                        <label>Estado <span class="text-danger">*</span></label>
+                        {{Form::select('estado', [
+                            'on'=>"on",
+                            'off'=>"off",
+                        ], null, ['class'=>"form-control", 'placeholder'=>"Estado"])}}
+                        @if($errors->has('estado'))
+                            <span class="text-danger">{{$errors->first('estado')}}</span>
+                        @endif
+                    </div>
+                   </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-12">
+                        {{Form::submit('Salvar', ['class'=>"btn btn-primary"])}}
+                    </div>
+                </div>
+
+                {{Form::close()}}
+            </div>
+
+        </div>
+    </div>
+</div>
+@endsection
