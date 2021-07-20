d752e2e5cbe888a08ae452da378436fb1c4f83fd
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-20 16:53:59 +0100

    terminado form de cadastro para escola

--- resources/views/admin/escola/create.blade.php
+++ resources/views/admin/escola/create.blade.php
@@ -5,7 +5,7 @@
         <div class="card-header">
         <div class="card-title">{{$submenu}}</div>
             <div class="card-category">
-            <a href="/admin/escolas/" class="btn btn-success">Listar<h2>{{$getEstudantes->count()}}</h2></a>
+            <a href="/admin/escolas/" class="btn btn-success">Listar</a>
             </div>
         </div>
         <div class="card-body">
@@ -32,22 +32,25 @@
                        </div>
                    </div>
 
-                   <div class="col-md-5">
+                   <div class="col-md-4">
                     <div class="form-group form-group-default">
-                        <label>Bilhete</label>
-                        {{Form::text('bilhete', null, ['class'=>"form-control", 'placeholder'=>"Nº do Bilhete"])}}
-                        @if($errors->has('bilhete'))
-                            <span class="text-danger">{{$errors->first('bilhete')}}</span>
+                        <label>Bairro <span class="text-danger">*</span></label>
+                        {{Form::text('bairro', null, ['class'=>"form-control", 'placeholder'=>"Bairro"])}}
+                        @if($errors->has('bairro'))
+                            <span class="text-danger">{{$errors->first('bairro')}}</span>
                         @endif
                     </div>
                    </div>
 
-                   <div class="col-md-4">
+                   <div class="col-md-3">
                     <div class="form-group form-group-default">
-                        <label>Telefone</label>
-                        {{Form::number('telefone', null, ['class'=>"form-control", 'placeholder'=>"Nº de Telefone"])}}
-                        @if($errors->has('telefone'))
-                            <span class="text-danger">{{$errors->first('telefone')}}</span>
+                        <label>Estado <span class="text-danger">*</span></label>
+                        {{Form::select('estado', [
+                            'on'=>"on",
+                            'off'=>"off"
+                        ], null, ['class'=>"form-control", 'placeholder'=>"Estado"])}}
+                        @if($errors->has('estado'))
+                        <span class="text-danger">{{$errors->first('estado')}}</span>
                         @endif
                     </div>
                    </div>
