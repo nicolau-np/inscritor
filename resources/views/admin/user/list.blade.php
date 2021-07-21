777637d6bce206529ecd6c32ff99e8c124d2af87
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-21 21:40:51 +0100

    trabalhando na view list usuarios

---
+++ resources/views/admin/user/list.blade.php
@@ -0,0 +1,48 @@
+@extends('layout.app')
+@section('content')
+<div class="col-md-12">
+    <div class="card">
+        <div class="card-header">
+        <div class="card-title">{{$submenu}}</div>
+            <div class="card-category">
+                <a href="/admin/cursos/create" class="btn btn-success">Novo</a>
+            </div>
+        </div>
+        <div class="card-body">
+
+            <div class="table">
+                <table class="table table-bordered table-head-bg-info table-bordered-bd-info mt-4">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Curso</th>
+                        <th scope="col">Estado</th>
+                        <th scope="col">Operações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($getCursos as $curso)
+
+                    <tr>
+                    <td>{{$loop->iteration}}</td>
+                    <td>{{$curso->curso}}</td>
+                    <td>{{$curso->estado}}</td>
+
+                        <td>
+                            <a href="/admin/cursos/{{$curso->id}}/edit" class="btn btn-primary btn-sm">Editar</a>
+                        </td>
+                    </tr>
+
+                    @endforeach
+                </tbody>
+            </table>
+
+            <div class="paginate">
+                {{$getCursos->links()}}
+            </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+@endsection
