628bb68ae13ecb296b960ce12662878f082ee9b3
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-21 22:27:58 +0100

    criada a view sobre

--- resources/views/admin/sobre.blade.php
+++ resources/views/admin/sobre.blade.php
@@ -1,64 +1,23 @@
 @extends('layout.app')
 @section('content')
-
-<div class="page-inner mt--5">
-    <div class="row mt--2">
-        <div class="col-md-6">
-            <div class="card full-height">
-                <div class="card-body">
-                    <div class="card-title">Overall statistics</div>
-                    <div class="card-category">Daily information about statistics in system</div>
-                    <div class="d-flex flex-wrap justify-content-around pb-2 pt-4">
-                        <div class="px-2 pb-2 pb-md-0 text-center">
-                            <div id="circles-1"></div>
-                            <h6 class="fw-bold mt-3 mb-0">New Users</h6>
-                        </div>
-                        <div class="px-2 pb-2 pb-md-0 text-center">
-                            <div id="circles-2"></div>
-                            <h6 class="fw-bold mt-3 mb-0">Sales</h6>
-                        </div>
-                        <div class="px-2 pb-2 pb-md-0 text-center">
-                            <div id="circles-3"></div>
-                            <h6 class="fw-bold mt-3 mb-0">Subscribers</h6>
-                        </div>
-                    </div>
-                </div>
-            </div>
+<style>
+    .tit{
+        font-weight: bold;
+    }
+</style>
+<div class="col-md-12">
+    <div class="card">
+        <div class="card-header">
+        <div class="card-title">{{$submenu}}</div>
+            <h4>Sobre o Sistema</h4>
         </div>
+        <div class="card-body">
 
-    </div>
-    <div class="row">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">
-                    <div class="card-head-row">
-                        <div class="card-title">User Statistics</div>
-                        <div class="card-tools">
-                            <a href="#" class="btn btn-info btn-border btn-round btn-sm mr-2">
-                                <span class="btn-label">
-                                    <i class="fa fa-pencil"></i>
-                                </span>
-                                Export
-                            </a>
-                            <a href="#" class="btn btn-info btn-border btn-round btn-sm">
-                                <span class="btn-label">
-                                    <i class="fa fa-print"></i>
-                                </span>
-                                Print
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div class="card-body">
-                    <div class="chart-container" style="min-height: 375px">
-                        <canvas id="statisticsChart"></canvas>
-                    </div>
-                    <div id="myChartLegend"></div>
-                </div>
-            </div>
+            <span class="tit">Autor:</span> Nicolau Ngala Pungue <br/>
+            <span class="tit">E-mail:</span> umn2017139241@student.umn.ed.ao <br/>
+            <span class="tit">E-mail:</span> nic340k@gmail.com <br/>
+            <span class="tit">Telef.:</span> +244 946 216 795 <br/>
         </div>
-
     </div>
-
 </div>
 @endsection
