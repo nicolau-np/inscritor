861ba7b20a0a1e96a88f20a9fb197d4e14bcc07c
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-24 13:03:31 +0100

    adicionando mais propriedades a view de listagem de estudantes

--- app/Http/Controllers/EstatisticaController.php
+++ app/Http/Controllers/EstatisticaController.php
@@ -2,17 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\AnoLectivo;
+use App\Curso;
+use App\Turno;
 use Illuminate\Http\Request;
 
 class EstatisticaController extends Controller
 {
     public function index(){
+        $cursos = Curso::pluck('curso', 'id');
+        $turnos = Turno::pluck('turno', 'id');
+        $ano_lectivos = AnoLectivo::pluck('ano_lectivo', 'id');
         $data = [
             'title' => "Estatísticas",
             'menu' => "Listas",
             'submenu' => "Listar",
             'type' => "estatisticas",
             'config' => null,
+            'getCursos' =>$cursos,
+            'getTurnos'=>$turnos,
+            'getAnosLectivos'=>$ano_lectivos,
         ];
         return view('admin.estatisticas.list', $data);
     }
--- resources/views/admin/estatisticas/list.blade.php
+++ resources/views/admin/estatisticas/list.blade.php
@@ -5,7 +5,7 @@
         <div class="card-header">
         <div class="card-title">{{$submenu}}</div>
             <div class="card-category">
-            
+
             </div>
         </div>
         <div class="card-body">
@@ -21,61 +21,6 @@
                 @endif
 
                 {{Form::open(['method'=>"post", 'url' =>"/admin/estudante"])}}
-               <div class="row">
-                   <div class="col-md-5">
-                       <div class="form-group form-group-default">
-                       <label>Nome completo <span class="text-danger">*</span></label>
-                       {{Form::text('nome', null, ['class'=>"form-control", 'placeholder'=>"Nome completo"])}}
-                       @if($errors->has('nome'))
-                        <span class="text-danger">{{$errors->first('nome')}}</span>
-                        @endif
-                       </div>
-                   </div>
-
-                   <div class="col-md-4">
-                    <div class="form-group form-group-default">
-                        <label>Gênero <span class="text-danger">*</span></label>
-                        {{Form::select('genero', [
-                            'M'=>"Masculino",
-                            'F'=>"Femenino",
-                        ], null, ['class'=>"form-control", 'placeholder'=>"Gênero"])}}
-                        @if($errors->has('genero'))
-                            <span class="text-danger">{{$errors->first('genero')}}</span>
-                        @endif
-                    </div>
-                   </div>
-
-                   <div class="col-md-3">
-                    <div class="form-group form-group-default">
-                        <label>Data de Nascimento <span class="text-danger">*</span></label>
-                        {{Form::date('data_nascimento', null, ['class'=>"form-control", 'placeholder'=>"Data de nascimento"])}}
-                        @if($errors->has('data_nascimento'))
-                            <span class="text-danger">{{$errors->first('data_nascimento')}}</span>
-                        @endif
-                    </div>
-                   </div>
-
-                   <div class="col-md-5">
-                    <div class="form-group form-group-default">
-                        <label>Bilhete</label>
-                        {{Form::text('bilhete', null, ['class'=>"form-control", 'placeholder'=>"Nº do Bilhete"])}}
-                        @if($errors->has('bilhete'))
-                            <span class="text-danger">{{$errors->first('bilhete')}}</span>
-                        @endif
-                    </div>
-                   </div>
-
-                   <div class="col-md-4">
-                    <div class="form-group form-group-default">
-                        <label>Telefone</label>
-                        {{Form::number('telefone', null, ['class'=>"form-control", 'placeholder'=>"Nº de Telefone"])}}
-                        @if($errors->has('telefone'))
-                            <span class="text-danger">{{$errors->first('telefone')}}</span>
-                        @endif
-                    </div>
-                   </div>
-                </div>
-
                 <div class="row">
                     <div class="col-md-4">
                         <div class="form-group form-group-default">
