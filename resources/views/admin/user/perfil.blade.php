aeeae36ac82d6ad9f2e29a80d513486acafe8054
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-24 12:10:27 +0100

    criando a view para perfil

---
+++ resources/views/admin/user/perfil.blade.php
@@ -0,0 +1,62 @@
+@extends('layout.app')
+@section('content')
+<div class="col-md-12">
+    <div class="card">
+        <div class="card-header">
+        <div class="card-title">{{$submenu}}</div>
+            <div class="card-category">
+
+            </div>
+        </div>
+        <div class="card-body">
+
+            <div class="form">
+
+                @if (session('error'))
+                    <div class="alert bg-danger" role="alert"><em class="fa fa-lg fa-warning">&nbsp;</em> {{__(session('error'))}} <a href="#" class="pull-right"><em class="fa fa-lg fa-close"></em></a></div>
+                @endif
+
+                @if (session('success'))
+                    <div class="alert bg-success" role="alert"><em class="fa fa-lg fa-check">&nbsp;</em> {{session('success')}} <a href="#" class="pull-right"><em class="fa fa-lg fa-close"></em></a></div>
+                @endif
+
+                {{Form::open(['method'=>"post", 'url' =>"/admin/usuarios/store"])}}
+                <div class="row">
+                    <div class="col-md-5">
+                        <div class="form-group form-group-default">
+                        <label>Palavra-Passe <span class="text-danger">*</span></label>
+                        {{Form::password('passe_antiga', null, ['class'=>"form-control", 'placeholder'=>"Palavra-Passe Actual"])}}
+                        @if($errors->has('passe_antiga'))
+                         <span class="text-danger">{{$errors->first('passe_antiga')}}</span>
+                         @endif
+                        </div>
+                    </div>
+
+                    <div class="col-md-5">
+                        <div class="form-group form-group-default">
+                        <label>Palavra-Passe Nova <span class="text-danger">*</span></label>
+                        {{Form::password('passe_nova', null, ['class'=>"form-control", 'placeholder'=>"Palavra-Passe Nova"])}}
+                        @if($errors->has('passe_nova'))
+                         <span class="text-danger">{{$errors->first('passe_nova')}}</span>
+                         @endif
+                        </div>
+                    </div>
+
+
+                 </div>
+
+
+
+                <div class="row">
+                    <div class="col-md-12">
+                        {{Form::submit('Salvar', ['class'=>"btn btn-primary"])}}
+                    </div>
+                </div>
+
+                {{Form::close()}}
+            </div>
+
+        </div>
+    </div>
+</div>
+@endsection
